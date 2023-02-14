e47362c6a4a0bdab1fd7d59a60f086dfd32b11a8
LennyN95 <lenny@moontec.de>
2023-02-14 09:45:04 +0100

    setup to use as (local) package

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+# install locally via `pip install -e .` (-> for development)
+# install from github via `pip install git+https://github.com/MHubAI/mhubio (-> in Dockerfiles)
+
+setup(
+    name = "mhubio",
+    version = "0.0.1",
+    author = "Leonard Nürnberg",
+    author_email = "lnuernberg@bwh.harvard.edu",
+    description = ("The glue layer framework used to harmonize MHub model's input and output data."),
+    license = "MIT",
+    keywords = "mhub",
+    url = "https://github.com/MHubAI/mhubio",
+    #packages=['an_example_pypi_project', 'tests'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
