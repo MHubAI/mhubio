c8180bd5614df60bd8499938088dee475b524b2e
LennyN95 <lenny@moontec.de>
2023-05-29 15:38:44 +0200

    support exporting probability maps in the nnunet runner module

--- mhubio/modules/runner/NNUnetRunner.py
+++ mhubio/modules/runner/NNUnetRunner.py
@@ -12,7 +12,9 @@ Email:  leonard.nuernberg@maastrichtuniversity.nl
 """
 # TODO: support multi-i/o and batch processing on multiple instances
 
+from typing import List
 import os, subprocess, shutil
+import SimpleITK as sitk, numpy as np
 from mhubio.core import Module, Instance, InstanceData, DataType, FileType, IO
 
 # TODO: add an optional evaluation pattern (regex) to IO.Config
@@ -24,6 +26,7 @@ nnunet_task_name_regex = r"Task[0-9]{3}_[a-zA-Z0-9_]+"
 @IO.Config('folds', int, None, the='number of folds to run nnunet on')
 @IO.Config('use_tta', bool, False, the='flag to enable test time augmentation')
 @IO.Config('export_prob_maps', bool, False, the='flag to export probability maps')
+@IO.Config('prob_map_segments', list, [], the='segment labels for probability maps')
 @IO.Config('roi', str, None, the='roi or comma separated list of roi the nnunet segments')
 class NNUnetRunner(Module):
 
@@ -33,7 +36,87 @@ class NNUnetRunner(Module):
     folds: int                          # TODO: support optional config attributes
     use_tta: bool
     export_prob_maps: bool
+    prob_map_segments: list
     roi: str
+
+    def export_prob_mask(self, nnunet_out_dir: str, ref_file: InstanceData, output_dtype: str = 'float32', structure_list: List[str] = []):
+        """
+        Convert softmax probability maps to NRRD. For simplicity, the probability maps
+        are converted by default to UInt8
+        Arguments:
+            model_output_folder : required - path to the folder where the inferred segmentation masks should be stored.
+            ref_file            : required - InstanceData object of the generated segmentation mask used as reference file.
+            output_dtype        : optional - output data type. Data type float16 is not supported by the NRRD standard,
+                                            so the choice should be between uint8, uint16 or float32. 
+            structure_list      : optional - list of the structures whose probability maps are stored in the 
+                                            first channel of the `.npz` file (output from the nnU-Net pipeline
+                                            when `export_prob_maps` is set to True). 
+        Outputs:
+            This function [...]
+        """
+
+        # sanity check user inputs
+        assert(output_dtype in ["uint8", "uint16", "float32"])      
+
+        # input file containing the raw information
+        pred_softmax_fn = 'VOLUME_001.npz'
+        pred_softmax_path = os.path.join(nnunet_out_dir, pred_softmax_fn)
+
+        # parse NRRD file - we will make use of if to populate the header of the
+        # NRRD mask we are going to get from the inferred segmentation mask
+        sitk_ct = sitk.ReadImage(ref_file.abspath)
+
+        # generate bundle for prob masks
+        # TODO: we really have to create folders (or add this as an option that defaults to true) automatically
+        prob_masks_bundle = ref_file.getDataBundle('prob_masks')
+        if not os.path.isdir(prob_masks_bundle.abspath):
+            os.mkdir(prob_masks_bundle.abspath)
+
+        # load softmax probability maps
+        pred_softmax_all = np.load(pred_softmax_path)["softmax"]
+
+        # iterate all channels
+        for channel in range(0, len(pred_softmax_all)):
+
+            structure = structure_list[channel] if channel < len(structure_list) else f"structure_{channel}"
+            pred_softmax_segmask = pred_softmax_all[channel].astype(dtype = np.float32)
+
+            if output_dtype == "float32":
+                # no rescale needed - the values will be between 0 and 1
+                # set SITK image dtype to Float32
+                sitk_dtype = sitk.sitkFloat32
+
+            elif output_dtype == "uint8":
+                # rescale between 0 and 255, quantize
+                pred_softmax_segmask = (255*pred_softmax_segmask).astype(np.int32)
+                # set SITK image dtype to UInt8
+                sitk_dtype = sitk.sitkUInt8
+
+            elif output_dtype == "uint16":
+                # rescale between 0 and 65536
+                pred_softmax_segmask = (65536*pred_softmax_segmask).astype(np.int32)
+                # set SITK image dtype to UInt16
+                sitk_dtype = sitk.sitkUInt16
+            else:
+                raise ValueError("Invalid output data type. Please choose between uint8, uint16 or float32.")
+                
+            pred_softmax_segmask_sitk = sitk.GetImageFromArray(pred_softmax_segmask)
+            pred_softmax_segmask_sitk.CopyInformation(sitk_ct)
+            pred_softmax_segmask_sitk = sitk.Cast(pred_softmax_segmask_sitk, sitk_dtype)
+
+            # generate data
+            prob_mask = InstanceData(f'{structure}.nrrd', DataType(FileType.NRRD, {'mod': 'prob_mask', 'structure': structure}), bundle=prob_masks_bundle)
+
+            # export file
+            writer = sitk.ImageFileWriter()
+            writer.UseCompressionOn()
+            writer.SetFileName(prob_mask.abspath)
+            writer.Execute(pred_softmax_segmask_sitk)
+
+            # check if the file was written
+            if os.path.isfile(prob_mask.abspath):
+                self.v(f" > prob mask for {structure} saved to {prob_mask.abspath}")
+                prob_mask.confirm()
 
     @IO.Instance()
     @IO.Input("in_data", IO.C("input_data_type"), the="input data to run nnunet on")
@@ -113,9 +196,9 @@ class NNUnetRunner(Module):
         # copy output data to instance
         shutil.copyfile(out_path, out_data.abspath)
 
+        # export probabiliy maps if requested as dynamic data
+        if self.export_prob_maps:
+            self.export_prob_mask(str(out_dir), out_data, 'float32', self.prob_map_segments)
+
         # update meta dynamically
         out_data.type.meta += meta
-
-        # confirm output data
-        if os.path.isfile(out_data.abspath):
-            out_data.confirm()
