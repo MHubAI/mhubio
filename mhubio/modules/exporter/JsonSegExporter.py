293383e9cc37eba9bdd9300b1c76646b94918794
LennyN95 <lenny@moontec.de>
2023-06-13 15:23:28 +0200

    align json seg exporter with data organizer

--- mhubio/modules/exporter/JsonSegExporter.py
+++ mhubio/modules/exporter/JsonSegExporter.py
@@ -11,39 +11,63 @@ Email:  leonard.nuernberg@maastrichtuniversity.nl
 -------------------------------------------------
 """
 
-from typing import List
-from mhubio.core import Module, IO, Instance, InstanceData, InstanceDataCollection, DataType
+from mhubio.core import Module, IO, Instance, InstanceData
 from mhubio.modules.organizer.DataOrganizer import DataOrganizer
 import json, os
 
-@IO.Config('source_segs', List[DataType], ['nifti:mod=seg:roi=*', 'nrrd:mod=seg:roi=*'], factory=IO.F.list(DataType.fromString), the='target segmentation files to convert to dicomseg')
+@IO.Config('target_dir', str, '', the="path prefix.")
+@IO.Config('targets', list, [], the="list of targets to organize the data into")
+@IO.Config('require_data_confirmation', bool, True, the="flag if set, only confirmed files will be exported by the organizer.")
 @IO.Config('segment_id_meta_key', str, 'roi', the='meta key used to identify the roi in the dicomseg json config file')
-@IO.Config('file_name_pattern', str, '[basename]', the="file name pattern which must match with the data organizer")
+
 class JsonSegExporter(Module):
 
-    source_segs: List[DataType]
+    target_dir: str
+    targets: list
+    require_data_confirmation: bool
     segment_id_meta_key: str
-    file_name_pattern: str
 
     @IO.Instance()
-    @IO.Inputs('in_datas', IO.C("source_segs"), the="data segmentation information is exported for")
     @IO.Output('out_data', path='segdef.json', dtype='json:mod=seg', the="json file containing segmentation information")
-    def task(self, instance: Instance, in_datas: InstanceDataCollection, out_data: InstanceData) -> None:
+    def task(self, instance: Instance, out_data: InstanceData) -> None:
 
         # generate json
         jseg = []
 
-        # iterate files
-        for in_data in in_datas:
+        # iterate target definitions
+        for target_definition in self.targets:
 
-            # get rois
-            rois = in_data.type.meta.getValue(self.segment_id_meta_key).split(',')
+            # extract source and target
+            src_def, tar_def = target_definition.split("-->")
 
-            # construct json
-            jseg.append({
-                'file': DataOrganizer.resolveTarget(self.file_name_pattern, in_data),
-                'labels': {(labelID+1): labelName for labelID, labelName in enumerate(rois)}
-            })
+            # filter for data type
+            in_datas = instance.data.filter(src_def, confirmed_only=self.require_data_confirmation)
+
+            # iterate data
+            for in_data in in_datas:
+
+                try: 
+
+                    # get segment_id_keys
+                    rois_def = in_data.type.meta.getValue(self.segment_id_meta_key)
+
+                    if not rois_def:
+                        raise Exception(f"Meta key {self.segment_id_meta_key} not provided / set.")
+
+                    # get rois
+                    rois = rois_def.split(',')
+
+                    # construct path
+                    in_data_path = os.path.join(self.target_dir, DataOrganizer.resolveTarget(tar_def, in_data))
+
+                    # construct json
+                    jseg.append({
+                        'file': in_data_path,
+                        'labels': {(labelID+1): labelName for labelID, labelName in enumerate(rois)}
+                    })
+
+                except Exception as e:
+                    self.v(f"Failed to export segmentation information for {str(in_data)} {in_data.abspath}: {e}")
 
         # store json
         with open(out_data.abspath, 'w') as f:
