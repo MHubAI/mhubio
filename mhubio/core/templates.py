459d192514d25289a8f8ec1e1d83c4e36aeabd97
LennyN95 <lenny@moontec.de>
2023-06-20 13:24:49 +0200

    update modality template to dicom values

--- mhubio/core/templates.py
+++ mhubio/core/templates.py
@@ -1,8 +1,11 @@
 from .Meta import Meta
 
-# define common types
-CT      = Meta(mod="ct")
-CBCT    = Meta(mod="cbct")
-MRI     = Meta(mod="mri")
-XRAY    = Meta(mod="xray")
-SEG     = Meta(mod="seg")
+# define common modality types
+CT      = Meta(mod="CT")        # Computed Tomography
+CBCT    = Meta(mod="CBCT")      # Cone Beam CT (Has DICOM a predefined modality value for cbct?))
+MR      = Meta(mod="MR")        # Magnetic Resonance
+RTDOSE  = Meta(mod="RTDOSE")    # Radiotherapy Dose
+PX      = Meta(mod="PX")        # Panoramic X-Ray
+XA      = Meta(mod="XA")        # X-Ray Angiography
+SEG     = Meta(mod="SEG")       # Segmentation
+US      = Meta(mod="US")        # Ultrasound
